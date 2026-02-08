0aeb3752bc5701975e5cc537a5b80e60356b5a82
scrungus <drevil629@gmail.com>
2026-02-08 13:14:15 +0000

    non-binding let

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -18,5 +18,5 @@ async fn health_check_works() {
 
 fn spawn_app() {
     let server = zero2prod::run().expect("Failed to bind address");
-    let _ = tokio::spawn(server);
+    tokio::spawn(server);
 }
