253eeb9c2b7d12d3760b612705082720e917e424
scrungus <drevil629@gmail.com>
2026-02-08 13:32:05 +0000

    inject port

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use actix_web::dev::Server;
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -10,14 +11,14 @@ async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             //.route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let default_port = "8000";
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", &default_port))
+        .unwrap_or_else(|_| panic!("Failed to bind port {}", &default_port));
+    run(listener)?.await
 }
